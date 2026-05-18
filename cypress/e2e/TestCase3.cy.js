edfa9771dffdfa568bfa1bdde59202485d720491
Paulo Varrone <paulorobertovarrone@gmail.com>
2026-05-18 18:31:17 -0300

    caso de teste get

---
+++ cypress/e2e/TestCase3.cy.js
@@ -0,0 +1,14 @@
+describe('Testes de API - Automation Exercise', () => {
+    
+    it('API 3 - pegar toda lista de marcas', () => {
+        cy.getAllBrandsList().then((response) => {
+            expect(response.status).to.eq(200);   
+            const body = JSON.parse(response.body);
+            expect(body).to.have.property('brands');
+            expect(body.brands).to.be.an('array');
+            expect(body.brands[0]).to.have.property('id');
+            expect(body.brands[0]).to.have.property('brand');   
+            expect(body.brands.length).to.be.greaterThan(0);
+        });
+    });
+});
