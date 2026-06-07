25774fe1fba87399a71a2649bfdfe0fffc6bf775
Paulo Varrone <paulorobertovarrone@gmail.com>
2026-06-07 00:15:53 -0300

    Refactor Cypress configuration: set viewport dimensions and project ID

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  projectId: "vo86wk",
+  
   e2e: {
     baseUrl: 'https://automationexercise.com',
     redirectionLimit: 100,
